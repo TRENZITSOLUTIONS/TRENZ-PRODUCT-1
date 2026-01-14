4c8699a08dd9461df1238974c90ba197b2690090
Rathinadevan E M <emrathinadev@gmail.com>
2026-01-14 18:39:01 +0530

    mior change

--- auth_app/views.py
+++ auth_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from .serializers import LoginSerializer, RegisterSerializer, ForgotPasswordSerializer, ResetPasswordSerializer
 from .models import Vendor
 
